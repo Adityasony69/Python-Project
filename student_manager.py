3246b01880a6c5813d4e41af2991048bec11cd0d
Aditya Soni <adityasony926@gmail.com>
2026-07-06 12:58:54 +0530

    1st push

---
+++ student_manager.py
@@ -0,0 +1,17 @@
+student = []
+
+#creating menu 
+
+print("===============================")
+print("Student Management System")
+print("===============================")
+print("1. Show Student")
+print("2. Add Student")
+print("3.Remove Student")
+print("4. Total Students")
+print("5. Exit")
+
+choice = input("Choose an option:")
+if choice =="1":
+    print(student)
+
