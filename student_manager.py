794af8682581018b2042b0c297a2b0454a13e31d
Adityasony69 <adityasony926@gmail.com>
2026-07-10 13:00:43 +0530

    adding search option

--- student_manager.py
+++ student_manager.py
@@ -7,12 +7,12 @@ def menu():
     print("Student Management System")
     print("=" *30)
     print("1. Show Student")
-    print("2. Add Student")
-    print("3. Remove Student")
-    print("4. Total Students")
-    print("5. Exit")
+    print("2. Search Student")
+    print("3. Add Student")
+    print("4. Remove Student")
+    print("5. Total Students")
+    print("6. Exit")
     
-menu()
 
 def show_students():
     print("=" * 30)
@@ -24,6 +24,13 @@ def add_student():
     student.append(name)
     print("Student added successfully.")
     print(student)
+    
+def search_student():
+    name = input("enter student name to  search:")
+    if name in student:
+        print("student is found in the list.")
+    else:
+        print(name + " student is not found in the list.")
     
 def remove_student():
     name = input ("enter student name to remove:")
@@ -38,19 +45,25 @@ def exit_program():
     print("Exiting the program. Goodbye!")
     
 
+running = True
+while running:
+    menu()
+    choice = input("Choose an option:")
+    if choice =="1":
+        show_students()
+    elif choice =="2":
+        search_student()
+    elif choice =="3":
+        add_student()
+    elif choice =="4":
+        remove_student()
+    elif choice =="5":
+        total_students()
+    elif choice =="6":
+      exit_program()
+      exit()
+    else:
+        print("Invalid choice. Please try again.")
+
     
 
-choice = input("Choose an option:")
-if choice =="1":
-    show_students()
-elif choice =="2":
-    add_student()
-    
-elif choice =="3":
-    remove_student()
-elif choice =="4":
-   total_students()
-elif choice =="5":
-      exit_program()
-else:
-    print("Invalid choice. Please try again.")
