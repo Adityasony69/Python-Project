f09277d696bc09f97afd5cf9a0d64e89ec0ada4e
Adityasony69 <adityasony926@gmail.com>
2026-07-10 17:39:33 +0530

    endin todays session

--- student_manager.py
+++ student_manager.py
@@ -1,4 +1,4 @@
-student = []
+student = ["aditya","karan","Rahul"]
 
 
 #creating menu 
@@ -11,10 +11,11 @@ def menu():
     print("3. Add Student")
     print("4. Remove Student")
     print("5. Total Students")
+    print("6. Update Students")
     print("6. Exit")
     
 
-def show_students():
+def display_students():
     print("=" * 30)
     print("List of Students:", student)
     
@@ -36,7 +37,7 @@ def add_student():
     else:
         student.append(new_name)
         print(f"✅ '{new_name}' added successfully.")
-        print(list_students())
+        return list_students()
     
 def search_student():
     name = input("enter student name to  search:")
@@ -47,13 +48,39 @@ def search_student():
     
 def remove_student():
     name = input ("enter student name to remove:")
-    student.remove(name)
-    print("Student removed successfully.")
+    if name in student:
+        student.remove(name)
+        print("Student removed successfully.")
+    else:
+        print(f"'{name}' was not found")
+        return
+    
     print(student)
     
 def total_students():
      print("Total no of students:", len(student))
-     
+ 
+ 
+def update_student():
+    name =input("enter student name to update:").strip().lower()
+    if name not in student:
+        print(f"'{name}' was not found in the list.")
+        return
+    updated = input("enter the updated name of the student:").strip().lower()
+    if updated == "":
+        print("Updated name cannot be empty.")
+        return
+    if updated in student:
+        print(f"'{updated}' already exists in the list.")
+        return
+    
+    position = student.index(name)
+    student[position] = updated
+    
+    print("Student Updated Sucessfully")
+    display_students()
+        
+        
 def exit_program():
     print("Exiting the program. Goodbye!")
     
@@ -63,7 +90,7 @@ while running:
     menu()
     choice = input("Choose an option:")
     if choice =="1":
-        show_students()
+        display_students()
     elif choice =="2":
         search_student()
     elif choice =="3":
@@ -73,6 +100,8 @@ while running:
     elif choice =="5":
         total_students()
     elif choice =="6":
+        update_student()
+    elif choice =="7":
       exit_program()
       exit()
     else:
